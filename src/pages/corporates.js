9deab98f4783b5d5f7a9ce62e186cd97116ba992
mutahhar1608 <231fa04003@gmail.com>
2025-07-04 00:02:00 +0530

    Create corporates.js

---
+++ src/pages/corporates.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Corporates = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Corporates Bookings</h2>
+      <p>We offer bulk booking, event planning, and customized solutions for corporates.</p>
+    </div>
+  );
+};
+
+export default Corporates;
