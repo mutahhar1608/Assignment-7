63fad940f4da857f4711eae9c1aaedfbe57130b0
mutahhar1608 <231fa04003@gmail.com>
2025-07-04 00:07:45 +0530

    Create offers.js

---
+++ src/pages/offers.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Offers = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Latest Offers</h2>
+      <ul>
+        <li>20% off with HDFC Credit Cards</li>
+        <li>Buy 1 Get 1 Free on select shows</li>
+        <li>Flat ₹150 cashback on Paytm payments</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Offers;
